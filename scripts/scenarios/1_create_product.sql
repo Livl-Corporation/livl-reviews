ea90672f186402ea37c6605897c46534b7f4bbfe
Julien Von Der Marck <vdmjulien05@gmail.com>
2023-06-30 14:11:30 +0200

    feat: update scenarios scripts

--- scripts/scenarios/1_create_product.sql
+++ scripts/scenarios/1_create_product.sql
@@ -1,8 +1,18 @@
-INSERT INTO Product (name, description, price, availableQuantity, published, link, createdOn, conditionId, createdBy)
-VALUES ('Stylo à plume original', 'Ce stylo est fabriqué en france.', 89.99, 5, true, 'https://amzn.to/43lfd2x', NOW(), 1, 3);
+DECLARE @newProductId INT;
+
+EXEC CreateNewProduct
+    @name = 'Stylo à plume original',
+    @description = 'Ce stylo est fabriqué en france.',
+    @price = 99.99,
+    @availableQuantity = 5,
+    @published = 1,
+    @link = 'https://amzn.to/43lfd2x',
+    @conditionId = 1,
+    @createdBy = 3,
+    @productId = @newProductId OUTPUT;
 
 INSERT INTO Media (caption, url)
 VALUES(NULL, "https://m.media-amazon.com/images/I/3133FKahikL._AC_SL1422_.jpg")
 
-INSERT INTO ProductMedia (productId, mediaId)
+INSERT INTO ProductMedia (@newProductId, mediaId)
 VALUES(3, 4)
--- scripts/scenarios/8_send_review.sql
+++ scripts/scenarios/8_send_review.sql
@@ -1,5 +1,14 @@
-INSERT INTO Review (note, title, message, createdOn, createdBy, relatedTo)
-VALUES (5, 'Great Product', 'This is a fantastic product ! Je vous écris en effet de ma plus belle plume pour attester que mon usage de ce stylo fut une expérience inestimable ! Mon nouveau livre "La stylographie" est bel et bien terminé et désormais disponible en librairie ;)', NOW(), 4, 3);
+DECLARE @reviewId INT;
+
+EXEC CreateReview
+    @note = 5,
+    @title = 'EGreat Product',
+    @message = 'This is a fantastic product ! Je vous écris en effet de ma plus belle plume pour attester que mon usage de ce stylo fut une expérience inestimable ! Mon nouveau livre "La stylographie" est bel et bien terminé et désormais disponible en librairie',
+    @createdBy = 4,
+    @relatedTo = 3,
+    @reviewId = @reviewId OUTPUT;
+
+SELECT @reviewId;
 
 INSERT INTO Media (caption, url)
 VALUES
@@ -8,8 +17,8 @@ VALUES
 
 INSERT INTO ReviewMedia(reviewId, mediaID)
 VALUES
-    (3, 5)
-    (3, 6)
+    (@reviewId, 5)
+    (@reviewId, 6)
 
 INSERT INTO RequestEvent (message, state, createdOn, requestId, createdBy)
 VALUES ("J'ai posté mon commentaire, merci beaucoup !", "reviewed", NOW(), 3, 3);
